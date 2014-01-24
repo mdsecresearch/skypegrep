e90d88858c6d3ddc42fa74b115f78bf993ee1ccf
mdsecresearch <dominic@mdsec.co.uk>
2014-01-24 15:27:52 +0000

    Mods to PacketSizeSymbol

--- src/skype/PacketSizeSymbol.java
+++ src/skype/PacketSizeSymbol.java
@@ -3,19 +3,19 @@ package skype;
 import org.biojava.bio.Annotation;
 import org.biojava.bio.symbol.FundamentalAtomicSymbol;
 
- public class PacketSizeSymbol extends FundamentalAtomicSymbol {
- private static final long serialVersionUID = -42925484312330097L;
+public class PacketSizeSymbol extends FundamentalAtomicSymbol {
 
- public PacketSizeSymbol(Integer num, Annotation annotation) {
- super(num.toString(), annotation);
- }
- @Override
- public boolean equals(Object obj) {
- return (obj instanceof PacketSizeSymbol) ? this.getName().equals(((PacketSizeSymbol) obj).getName()) : false;
- }
+    public PacketSizeSymbol(Integer num, Annotation annotation) {
+        super(num.toString(), annotation);
+    }
 
- @Override
- public int hashCode() {
- return getName().hashCode();
- }
- }
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof PacketSizeSymbol) ? this.getName().equals(((PacketSizeSymbol) obj).getName()) : false;
+    }
+
+    @Override
+    public int hashCode() {
+        return getName().hashCode();
+    }
+}
